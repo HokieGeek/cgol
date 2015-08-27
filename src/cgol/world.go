9106ee247c144acaa1811d70b965d366890f9f66
HokieGeek <andres.f.perez@gmail.com>
2015-08-27 05:38:39 -0400

    Added a comment :)

--- src/cgol/world.go
+++ src/cgol/world.go
@@ -1,13 +1,17 @@
 package cgol
 
+/*
+TODO: maybe if a pond reaches "critical mass" a portion of the organisms move out into a new pond using the same strategy?
+*/
+
 type World struct {
-    ponds []Strategy
+	ponds []Strategy
 }
 
 func (t *World) Ponds() {
-    // TODO
+	// TODO
 }
 
 func (t *World) String() string {
-    return "TODO"
+	return "TODO"
 }
