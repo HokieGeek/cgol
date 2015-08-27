385de50d74a4484d5d9f753389b3201d5541ce14
HokieGeek <andres.f.perez@gmail.com>
2015-08-27 06:06:46 -0400

    Blocked out a couple of inits of common shapes for testing a processor

--- src/cgol/inits.go
+++ src/cgol/inits.go
@@ -22,10 +22,47 @@ func InitRandom(pond *Pond, percent int) []GameboardLocation {
 	return initialLiving
 }
 
-func Blocks(pond *Pond, size int) []GameboardLocation {
+func Blinkers(pond *Pond) []GameboardLocation {
+	// length := 3
+
 	initialLiving := make([]GameboardLocation, 0)
 
-	// TODO: put in as many sizexsize blocks as you can fit
+	// TODO: put in as many lengthx1 vertical lines as you can fit
+	// Period 1
+	// -1-
+	// -1-
+	// -1-
+	// Period 2
+	// ---
+	// 111
+	// ---
+	//
+	// 1. Determine how many can fit vertically (rows / length+1) [+1 == spacer]
+	// 2. Determine how many can fit horizontally (cols / length+1)
+	// 3. Determine each viable centerPoint
+	//    Really just need the very first one, after that I need to do
+	//    centerPoint.X + width for each col and
+	//    centerPoint.Y + height for each row
+	// 4. For each centerPoint
+	//    1. initialiLiving = append(initialLiving, centerPoint)
+	//    2. initialiLiving = append(initialLiving, centerPoint.Y-1)
+	//    3. initialiLiving = append(initialLiving, centerPoint.Y+1)
+
+	return initialLiving
+}
+
+func Toads(pond *Pond) []GameboardLocation {
+	initialLiving := make([]GameboardLocation, 0)
+
+	// TODO
+	// Period 1
+	// -111
+	// 111-
+	// Period 2
+	// --1-
+	// 1--1
+	// 1--1
+	// -1--
 
 	return initialLiving
 }
