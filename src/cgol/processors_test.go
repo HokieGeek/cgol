7d4c09a3c2b97c70b303a5ef3f2cb2dcfc73bd41
HokieGeek <andres.f.perez@gmail.com>
2015-08-29 17:21:35 -0400

    Added toads unit test

--- src/cgol/processors_test.go
+++ src/cgol/processors_test.go
@@ -26,5 +26,36 @@ func TestProcessorSimultaneousStandardBlinker(t *testing.T) {
 	if !pond.gameboard.Equals(expected) {
 		t.FailNow()
 	}
+}
 
+func TestProcessorSimultaneousStandardToad(t *testing.T) {
+	size := GameboardDims{Height: 4, Width: 4}
+
+	// Build the initial pond
+	pond := NewPond(size.Height, size.Width, NEIGHBORS_ALL)
+	initial := make([]GameboardLocation, 6)
+	initial[0] = GameboardLocation{X: 1, Y: 1}
+	initial[1] = GameboardLocation{X: 2, Y: 1}
+	initial[2] = GameboardLocation{X: 3, Y: 1}
+	initial[3] = GameboardLocation{X: 0, Y: 2}
+	initial[4] = GameboardLocation{X: 1, Y: 2}
+	initial[5] = GameboardLocation{X: 2, Y: 2}
+	pond.init(initial)
+
+	// Build the expected gameboard
+	expected := NewGameboard(size)
+	expected.SetGameboardValue(GameboardLocation{X: 2, Y: 0}, 0)
+	expected.SetGameboardValue(GameboardLocation{X: 0, Y: 1}, 0)
+	expected.SetGameboardValue(GameboardLocation{X: 3, Y: 1}, 0)
+	expected.SetGameboardValue(GameboardLocation{X: 0, Y: 2}, 0)
+	expected.SetGameboardValue(GameboardLocation{X: 3, Y: 2}, 0)
+	expected.SetGameboardValue(GameboardLocation{X: 1, Y: 3}, 0)
+
+	// Go through one generation
+	SimultaneousProcessor(pond, Standard)
+
+	// Compare the pond with the expected version
+	if !pond.gameboard.Equals(expected) {
+		t.FailNow()
+	}
 }
