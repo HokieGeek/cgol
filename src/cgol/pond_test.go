05247346a504cefd2ae482b141f08eba451af9c8
HokieGeek <andres.f.perez@gmail.com>
2015-08-29 23:11:05 -0400

    Pusing up a placeholder for the pond unit tests

---
+++ src/cgol/pond_test.go
@@ -0,0 +1,23 @@
+package cgol
+
+import "testing"
+
+func TestPondSettingInitialPatterns(t *testing.T) {
+	// TODO
+}
+
+func TestPondNeighborSelection(t *testing.T) {
+	// TODO
+}
+
+func TestPondOrganismValue(t *testing.T) {
+	// TODO
+}
+
+func TestPondGetNumLiving(t *testing.T) {
+	// TODO
+}
+
+func TestPondNeighborCountCalutation(t *testing.T) {
+	// TODO
+}
