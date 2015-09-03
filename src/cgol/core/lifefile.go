4f7e4f8c3ad360bd2c4fbc4ac20afb61ca01b626
HokieGeek <andres.f.perez@gmail.com>
2015-09-03 15:52:30 -0400

    Adding file reader skeleton per #10

---
+++ src/cgol/core/lifefile.go
@@ -0,0 +1,35 @@
+package cgol
+
+// import "fmt"
+
+/*
+func readLifeV1.05(FILE) (*Pattern, error) {
+#Life 1.06
+0 -1
+1 0
+-1 1
+0 1
+1 1
+}
+
+func readLifeV1.05(FILE) (*Pattern, error) {
+#Life 1.05
+#D This is a glider.  [comments]
+#N  [optional. see below]
+#P -1 -1  [can't quite get this...]
+.*.
+..*
+***
+
+
+#N == #R 23/3
+
+#R 125/36 ->  the pattern should be run in a universe where 1, 2, or 5 neighbors are necessary for a cell's survival, and 3 or 6 neighbours allow a cell to come alive.
+}
+
+func Read(FILE) (*Pattern, error) {
+}
+
+func Write(Pattern, FILE) error {
+}
+*/
