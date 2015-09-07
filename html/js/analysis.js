f2ccb4af26949241dfc19230739fa017f13a3c3b
HokieGeek <andres.f.perez@gmail.com>
2015-09-07 09:25:44 -0400

    Well, I'm closer to displaying a board on the page but still need to think how it will be managed on the life server

--- html/js/analysis.js
+++ html/js/analysis.js
@@ -1,13 +1,24 @@
 //  function add
 var analyses = {}
 
+function getIdStr(id) {
+    // console.log("getIdStr():", id);
+    // console.log("getIdStr():", id.toString(16));
+    // console.log("getIdStr():", parseInt(id, 16));
+    // console.log("getIdStr():", parseInt(id, 16).toString(16));
+    var idStr = id.toString(16).replace(new RegExp("[/+=]", 'g'), "");
+    return idStr.substring(0, idStr.length-1);
+}
+
 function createBoard(data) {
+    var idStr = getIdStr(data.Id);
+
     var span = $("<span></span>");
     for (var i = 0; i < data.Dims.Height; i++) {
         var row = $("<div></div>");
         for(var j = 0; j < data.Dims.Width; j++) {
             row.append($("<span></span>")
-                    .attr("id", "cell-"+data.Id+"-"+i+"x"+j)
+                    .attr("id", "cell-"+idStr+"-"+i+"x"+j)
                     .addClass("analysisBoardCell")
                     .html("&nbsp"))
         }
@@ -18,14 +29,17 @@ function createBoard(data) {
 
 function createAnalysis(data) {
     console.log("createAnalysis()", data)
+
+    var idStr = getIdStr(data.Id);
+
     $("#analyses").html(
-        $("<div></div>").attr("id", "analysis-"+data.Id.toString(16))
+        $("<div></div>").attr("id", "analysis-"+idStr)
 
         // ID
-        .append($("<div></div>")
+        .append($("<div></div>").addClass("analysisField")
                 .append("<span>ID: </span>")
-                .append($("<span></span>").addClass("analysisId").text(data.Id.toString(16))))
-                .addClass("analysisField")
+                .append($("<span></span>").addClass("analysisId").text(idStr)))
+
 
         // Rule
         // .append($("<div></div>")
@@ -34,44 +48,63 @@ function createAnalysis(data) {
         //         .addClass("analysisField").addClass("analysisRule"))
 
         // Neighbors
-        .append($("<div></div>")
+        .append($("<div></div>").addClass("analysisField")
                 .append("<span>Neighbors: </span>")
                 .append($("<span></span>").addClass("analysisNeighbors").text("TODO")))
-                .addClass("analysisField")
 
         // Status
-        .append($("<div></div>")
+        .append($("<div></div>").addClass("analysisField")
                 .append("<span>Status: </span>")
-                .append($("<span></span>").addClass("analysisStatus")))
-                .addClass("analysisField")
+                .append($("<span></span>").text("Unknown")
+                    .attr("id", "status-"+idStr)
+                    .addClass("analysisStatus")))
 
         // Generation
-        .append($("<div></div>")
+        .append($("<div></div>").addClass("analysisField")
                 .append("<span>Generation: </span>")
-                .append($("<span></span>").addClass("analysisGeneration")))
-                .addClass("analysisField")
+                .append($("<span></span>").text("0")
+                    .attr("id", "generation-"+idStr)
+                    .addClass("analysisGeneration")))
 
         // Living cells
-        .append($("<div></div>").attr("class", "analysisBoard").html(createBoard(data)))
-    )
+        .append($("<div></div>").attr("class", "analysisBoard").attr("id", "board-"+idStr)
+                .html(createBoard(data)))
+    );
 
-    analyses[data.Id] = data
+    analyses[data.Id] = data;
 }
 
 function updateBoard(data) {
+    console.log("updateBoard()", data);
+
+    var idStr = getIdStr(data.Id);
+    for (var i = data.Living.length-1; i >= 0; i--) {
+        var living = data.Living[i];
+        console.log("Living["+i+"]: ", living);
+        console.log("   ID:", "#cell-"+idStr+"-"+living.Y+"x"+living.X)
+        $("#cell-"+idStr+"-"+living.Y+"x"+living.X).addClass("analysisBoardCellAlive");
+    }
     // TODO
 }
 
 function updateAnalysis(data) {
-    console.log("updateAnalysis()", data)
+    console.log("updateAnalysis()", data);
+
+    var idStr = getIdStr(data.Id);
+
+    var update = data.Updates[0];
+    $("#status-"+idStr).text(update.Status);
+    $("#generaton-"+idStr).text(update.Generation);
+
     // TODO: Loop through the updates
+    updateBoard(update);
 }
 
 function newAnalysisData(data) {
     if (data.Id in analyses) {
-        updateAnalysis(data)
+        updateAnalysis(data);
     } else {
-        createAnalysis(data)
+        createAnalysis(data);
     }
 }
 
@@ -91,7 +124,7 @@ function createNewAnalysis() {
             JSON.stringify({"Dims":{"Height": 30, "Width": 200}}))
   .done(function( data ) {
       createAnalysis(data);
-      pollAnalyses()
+      pollAnalyses();
   });
 }
 
